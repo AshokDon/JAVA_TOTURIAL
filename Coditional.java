221ec8021ca9cbf47e62d8409e4f825fb69406d9
Ashok <68994450+AshokDon@users.noreply.github.com>
2023-07-17 23:09:27 +0530

    Add files via upload

---
+++ Coditional.java
@@ -0,0 +1,31 @@
+//conditional statements
+public class Coditional {
+    public static void main(String a[]){
+        // int x = 10;
+        // if(x >= 10){
+        //     System.out.println("hello");
+        // }
+        // else{
+        //     System.out.println("bye");
+        // }
+
+        //Ternary operator
+        // int n = 4;
+        // int result = (n%2==0)?10:20;
+
+        // System.out.println(result);
+
+        //Switch
+        int x = 1;
+        switch (x) {
+            case 1:
+                System.out.println("monday");
+                break;
+            case 2:
+                System.out.println("Tusday");
+            default:
+                System.out.println("worng value");
+                break;
+        }
+    }
+}
