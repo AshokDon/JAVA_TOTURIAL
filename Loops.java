ed6b27c9282e21b0bf020681f01986a6ac78f9d2
Ashok <68994450+AshokDon@users.noreply.github.com>
2023-08-08 21:37:31 +0530

    Add files via upload

---
+++ Loops.java
@@ -0,0 +1,14 @@
+public class Loops {
+    public static void main(String a[]){
+        int i = 5;
+        // while(i <= 4){
+        //     System.out.println("hi"+i);
+        //     i++;
+        // }
+
+        do{
+            System.out.println("Hi"+i);
+            i++;
+        }while(i<=4);
+    }  
+}
